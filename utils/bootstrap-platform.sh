7d12dc5808f9e1fe816f4370daed5b0b78159757
benhur-araujo <benhur.araujo.silva@gmail.com>
2024-06-27 10:28:38 -0300

    feat(utils): add script to bootstrap platform

---
+++ utils/bootstrap-platform.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+AZ_SUBSCRIPTION="conquerproject"
+RESOURCE_GROUP="rg-conquerplatform-dev-01"
+AKS_NAME="aks-conquerproject-dev-01"
+TERRAGRUNT_CONFIG="$(git rev-parse --show-toplevel)/infrastructure/azure/dev/ephemeral/aks/terragrunt.hcl"
+BOOTSTRAP_ARGOCD="$(git rev-parse --show-toplevel)/argocd/argocd-bootstrap/argocd-bootstrap.sh"
+
+# Set Azure Subscription
+az account set --subscription "$AZ_SUBSCRIPTION"
+
+usage() {
+    echo "Usage: $0 [--apply] [--destroy]"
+    exit 1
+}
+
+bootstrap_platform() {
+    printf "Provisioning AKS and setting context on kubeconfig file...\n\n"
+    terragrunt --terragrunt-config "$TERRAGRUNT_CONFIG" apply --auto-approve
+    az aks get-credentials --resource-group "$RESOURCE_GROUP" --name "$AKS_NAME" --overwrite-existing --admin
+
+    printf "Deploying ArgoCD...\n\n"
+    bash "$BOOTSTRAP_ARGOCD"
+}
+
+destroy_platform() {
+    printf "Destroying AKS...\n\n"
+    terragrunt --terragrunt-config "$TERRAGRUNT_CONFIG" destroy --auto-approve
+}
+
+main() {
+    case "$1" in
+        --apply) bootstrap_platform; shift;;
+        --destroy) destroy_platform; shift;;
+        *) usage;;
+    esac
+}
+
+main "$1"
